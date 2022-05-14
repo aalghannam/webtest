f3d0fca839d06841ec8aeb2746801e5a20496e70
aalghannam <abdulrahman.a.alghannam@gmail.com>
2022-05-14 13:50:24 +0300

    first commit

---
+++ app/scrept.js
@@ -0,0 +1,17 @@
+// var num = window.prompt("plz ent num")
+
+// if (Number(num % 2==0)){
+// console.log("ev")
+// }else{
+//     console.log("odd")
+// }
+
+var x = ''
+
+for(var i =0;i<10;i++){
+    x += `<div >
+    <img src="clem-onojeghuo-206832-600x500.jpg" alt="">
+</div>` 
+}
+
+document.getElementById("demo").innerHTML = x;
